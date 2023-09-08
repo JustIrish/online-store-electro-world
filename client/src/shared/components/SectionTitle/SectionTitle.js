2f2d6a8aef9279e6754ee11c722fb2cfc0cad4de
Irina Khorunz <irahorunz87@gmail.com>
2023-09-09 01:09:48 +0300

    add component SectionTitle

---
+++ client/src/shared/components/SectionTitle/SectionTitle.js
@@ -0,0 +1,7 @@
+import { StyledTitle } from './SectionTitle.styled';
+
+const SectionTitle = ({ title }) => {
+  return <StyledTitle>{title}</StyledTitle>;
+};
+
+export default SectionTitle;
---
+++ client/src/shared/components/SectionTitle/SectionTitle.styled.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const StyledTitle = styled.h2`
+  font-family: 'Sansation Regular';
+  font-size: 54px;
+  font-weight: 400;
+  line-height: 1.009;
+`;
